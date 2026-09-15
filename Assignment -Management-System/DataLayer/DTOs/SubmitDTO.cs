e6d628a07c764bb7c261280a2e50a1cf99eac939
Yusef <youssefmohmed430@gmail.com>
2026-09-15 14:38:38 +0300

    feat: support file uploads in assignment submissions

--- Assignment -Management-System/DataLayer/DTOs/SubmitDTO.cs
+++ Assignment -Management-System/DataLayer/DTOs/SubmitDTO.cs
@@ -1,12 +1,15 @@
-﻿namespace Assignment__Management_System.DataLayer.DTOs
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Assignment__Management_System.DataLayer.DTOs
 {
     public class SubmitDTO
     {
-        public string FileName { get; set; }
+        public IFormFile? File { get; set; }
+        public string? FileName { get; set; }
         public int AssignmentId { get; set; }
         public string? AssignmentTitle { get; set; }
-        public double grade { get; set; }
+        public double? grade { get; set; }
         public string? stuname { get; set; }
-
+        public int? SubmissionId { get; set; }
     }
 }
