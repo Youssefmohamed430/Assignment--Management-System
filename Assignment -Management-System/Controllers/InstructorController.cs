e8ef08a64a242b3c4bf116fe28384e99b866ba07
Yusef <youssefmohmed430@gmail.com>
2026-09-17 11:56:10 +0300

    Add instructor assignment file upload and download endpoint

--- Assignment -Management-System/Controllers/InstructorController.cs
+++ Assignment -Management-System/Controllers/InstructorController.cs
@@ -3,10 +3,7 @@ using Assignment__Management_System.Models.Entities;
 using Assignment__Management_System.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
-using System;
 using System.Security.Claims;
-using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.AspNetCore.Authorization;
 
 namespace Assignment__Management_System.Controllers
@@ -21,51 +18,66 @@ namespace Assignment__Management_System.Controllers
         {
             _instructorService = instructorService;
         }
+
         [Authorize(Roles = "Instructor")]
         [HttpGet("InstructorCourses")]
-        public IActionResult GetInstructorCourses() 
+        public IActionResult GetInstructorCourses()
         {
             var instid = User.FindFirstValue("uid");
-
             var result = _instructorService.GetInstructorCourses(instid);
-
             return result.IsSuccess ? Ok(result) : BadRequest(result);
         }
+
         [Authorize(Roles = "Instructor")]
         [HttpPost]
-        public IActionResult AddAssignmentToCourse(AssignmentDTO assignment)
+        [RequestSizeLimit(10 * 1024 * 1024)]
+        public IActionResult AddAssignmentToCourse([FromForm] AssignmentDTO assignment)
         {
             if (!ModelState.IsValid)
-                return BadRequest(ModelState);  
+                return BadRequest(ModelState);
 
             var userid = User.FindFirstValue("uid");
-            
             var result = _instructorService.AddAssignmentToCourse(userid, assignment);
 
             return result.IsSuccess ? Ok(result) : BadRequest(result);
         }
+
+        [Authorize(Roles = "Instructor")]
+        [HttpGet("file/{assignmentId}")]
+        public IActionResult GetAssignmentFile(int assignmentId)
+        {
+            var result = _instructorService.GetAssignmentFile(assignmentId);
+
+            if (!result.IsSuccess)
+                return NotFound(result);
+
+            return File(
+                result.Value.FileBytes,
+                result.Value.ContentType,
+                result.Value.FileName);
+        }
+
         [Authorize(Roles = "Instructor")]
         [HttpPut]
         public IActionResult UpdateAssignmentsGrades([FromQuery]int Subid ,[FromQuery] double grade)
         {
             var result = _instructorService.UpdateAssignmentsGrades(Subid,grade);
-
             return result.IsSuccess ? Ok(result) : BadRequest(result);
         }
+
         [Authorize(Roles = "Instructor")]
         [HttpGet("{assignmentid}")]
         public IActionResult GetAssignmentStudentGrades(int assignmentid)
         {
             var result = _instructorService.GetAssignmentStudentGrades(assignmentid);
-
             return result.IsSuccess ? Ok(result) : BadRequest(result);
         }
+
         [Authorize(Roles = "Admin")]
         [HttpGet]
         public IActionResult GetInstructors()
         {
             var result = _instructorService.GetInstructors();
-
             return result.IsSuccess ? Ok(result) : BadRequest(result);
         }
     }
